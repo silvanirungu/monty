7a3a70b6ae3eae64ea781c6f0b47b8227c3a304a
Moses Mumo <mosesm5499@gmail.com>
2023-10-22 23:49:07 +0300

    Update verify.c

--- verify.c
+++ verify.c
@@ -1,17 +1,17 @@
 #include "monty.h"
-
+             
 arg_t arg = {0, 0};
-
-/**
+        
+/**       
  * comment_check - checks if line is a comment
  * @line: struct containing line contents and line number
- *
+ *      
  * Return: true if comment, else false
- */
+ */      
 bool comment_check(line_t line)
 {
 	if (!line.content[0])
-	{
+	{     
 		free(line.content);
 		return (true);
 	}
