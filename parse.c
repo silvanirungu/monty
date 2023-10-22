46aa18f2789c3d5374e4fb7c508fbeb2572cf970
Moses Mumo <mosesm5499@gmail.com>
2023-10-22 23:48:25 +0300

    Update parse.c

--- parse.c
+++ parse.c
@@ -1,12 +1,12 @@
 #include "monty.h"
 
-/**
+/**      
  * parseline - tokenizes a line of text, storing it in line struct
  * @line: struct containing line contents and line number
  * @buffer: string of text read from script file
- *
+ *           
  * Return: Nothing
- */
+ */       
 void parseline(line_t *line, char *buffer)
 {
 	unsigned int i;
@@ -18,14 +18,14 @@ void parseline(line_t *line, char *buffer)
 		fprintf(stderr, "Error: malloc failed");
 		exit(EXIT_FAILURE);
 	}
-
+         
 	token = strtok(buffer, " '\n'");
 	for (i = 0; token && i < 2; i++)
 	{
 		line->content[i] = token;
 		token = strtok(NULL, " \n");
 	}
-
+         
 	line->content[i] = NULL;
 }
 
