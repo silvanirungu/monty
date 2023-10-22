310fb2772582abac2c417651a719498167cdf6b3
Moses Mumo <mosesm5499@gmail.com>
2023-10-22 23:44:12 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -7,16 +7,16 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <ctype.h>
-
+        
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- *
+ *             
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
- */
+ */      
 typedef struct arg_s
 {
 	int arg;
