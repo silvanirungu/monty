303b8cd9ef0edebb0053c8a98c126dd4d9e82ad9
Moses Mumo <mosesm5499@gmail.com>
2023-10-22 23:46:46 +0300

    Update opcodes_math.c

--- opcodes_math.c
+++ opcodes_math.c
@@ -1,21 +1,21 @@
 #include "monty.h"
 
-/**
+/**       
  * addop - adds the top two elements together
  * @stack: pointer to the head node pointer of stack
  * @nline: the line number
- * Return: Nothing.
+ * Return: Nothing.      
  */
 void addop(stack_t **stack, unsigned int nline)
 {
-	stack_t *temp;
+	stack_t *temp;      
 
 	if (!(*stack) || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", nline);
 		exit(EXIT_FAILURE);
 	}
-
+          
 	temp = *stack;
 	(*stack)->next->n += (*stack)->n;
 	*stack = (*stack)->next;
