d01d7c1085c6c4b9c2b84af59bcbef495d96b6c5
Moses Mumo <mosesm5499@gmail.com>
2023-10-22 23:47:41 +0300

    Update opcodes_mode.c

--- opcodes_mode.c
+++ opcodes_mode.c
@@ -1,20 +1,20 @@
 #include "monty.h"
-
+         
 /**
  * addst - changes mode of push to stack
  * @stack: pointer to stack
  * @nline: line number
- *
+ *            
  * Return: Nothing.
- */
+ */            
 void addst(stack_t **stack, unsigned int nline)
-{
-	(void)nline;
+{       
+	(void)nline;      
 	(void)stack;
 
 	arg.flag = 0;
 }
-
+           
 /**
  * addqu - changes mode of push to queue
  * @stack: pointer to stack
